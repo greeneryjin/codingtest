a48dda29a616593984b7e9d82f34f8d1906d2242
Jin <87289562+greeneryjin@users.noreply.github.com>
2023-03-22 15:56:14 +0900

    Update 기사단원의 무기.java

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -16,11 +16,17 @@ class Solution {
         return answer;
     }
     
+    //약수를 구하는 매서드 
     public int solution(int x) {
         int count = 0;
         
+        //제곱근 계산 -> 제곱근은 약수의 개수의 절반을 차지.  
         for(int i = 1; i <= Math.sqrt(x); i++) {
+            
+            //해당 수와 제곱근 나머지가 0이면 약수 
             if(x % i == 0) {
+                
+                //제곱근과 해당 수를 나눈 값이 제곱근이 같으면 약수 
                 if(x / i == i) {
                     count++;
                 } else {
