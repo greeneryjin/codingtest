d28c73da3eec15c0bb59341a7dc7866dab52a730
Jin <87289562+greeneryjin@users.noreply.github.com>
2023-03-22 16:03:30 +0900

    Update 크기가 작은 부분문자열.java

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -2,8 +2,11 @@ class Solution {
     public int solution(String t, String p) {
         int answer = 0;
         long number = Long.parseLong(p);
+        
+        //outOfArray 방지 및 p 문자열만큼 뽑기 
         int n = t.length() - p.length();
         int index = p.length();
+       
         for(int i = 0; i <= n; i++) {
             String tmp = t.substring(i,i + index);
             long tmps = Long.parseLong(tmp);
