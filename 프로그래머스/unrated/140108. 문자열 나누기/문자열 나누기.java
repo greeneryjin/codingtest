b9c4d378370464292f908cd09dc869e9bb733a8e
Jin <87289562+greeneryjin@users.noreply.github.com>
2023-03-22 16:00:43 +0900

    Update 문자열 나누기.java

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -5,12 +5,14 @@ class Solution {
         int notCount = 0;
         for(int i = 0; i < s.length(); i++) {
             for(int j = i; j < s.length(); j++) {
+                
+                //배열 i와 배열 j를 비교하고 같은 값과 다른 값을 구함 
                 if(s.charAt(i) == s.charAt(j)) {
                     sameCount++;
                 } else {
                     notCount++;
                 }
-                
+                //같은 값과 다른 값이 같으면 i를 j로 이동 && j 길이가 전체 길이를 벗어나지 않도록 작업 
                 if(sameCount == notCount || j == s.length() - 1) {
                     i = j+1;
                     answer++;
