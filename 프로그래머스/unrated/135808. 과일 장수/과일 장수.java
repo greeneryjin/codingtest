2135d6030e657af272562c163afa0ce4867323c8
Jin <87289562+greeneryjin@users.noreply.github.com>
2023-03-22 15:51:06 +0900

    Update 과일 장수.java

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -3,6 +3,8 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
+        
+        // 내림차순 정렬 
         Integer[] arr = Arrays.stream(score).boxed().toArray(Integer[]::new);
         Arrays.sort(arr, Collections.reverseOrder());
         int len = score.length;
@@ -12,13 +14,16 @@ class Solution {
         
         while(x < len) {
             if(len - index >= m) {
+                //m개 만큼 임시 배열에 저장
                 for(int i = 0; i < tmp.length; i++) {
                     if(index == len) {
                         break;
-                    }
+                    }//임시 배열에 값 저장
                     tmp[i] = arr[index++];
                 }
+                //오름차순 정렬 
                 Arrays.sort(tmp);
+                //가장 큰 값은 항상 맨 앞. 
                 answer += (m * 1 * tmp[0]);
                 x += m;
             }
