8a12dfa87183f377ce6bf37d3f4a36763f896207
Jin <87289562+greeneryjin@users.noreply.github.com>
2023-03-22 16:02:02 +0900

    Update 가장 가까운 같은 글자.java

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -2,6 +2,8 @@ class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
         answer[0] = -1;
+        
+        //배열 뒤쪽을 시작으로 배열 맨 뒤 -> 맨 앞의 순서로 문자열 찾기 
         for(int i = s.length() - 1; i > 0; i--) {
             for(int j = i - 1; j >= 0; j--) {
                 if(s.charAt(i) != s.charAt(j)) {
