45d41f5b7af203857fce51c8aca2ac63e1f1c97e
Jin <87289562+greeneryjin@users.noreply.github.com>
2023-03-22 15:52:55 +0900

    Update 푸드 파이트 대회.java

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -3,15 +3,19 @@ class Solution {
         StringBuffer buff = new StringBuffer();
         
         for(int i = 1; i < food.length; i++) {
+            //배열의 값을 두 사람으로 나눈 값을 저장 
             int x = food[i] / 2;
+            //음식이 나눌 수 없을 경우, 다음
             if(x == 0) continue;
             else {
+                //나눈 값만큼 배열에 저장 
                 for(int j = 0; j < x; j++) {
                     buff.append(Integer.toString(i));
                 }
             }
         }
         String answer = buff.toString() + "0";
+        //배열 뒤집기 
         String reversedStr = buff.reverse().toString();
         return answer + reversedStr;
     }
