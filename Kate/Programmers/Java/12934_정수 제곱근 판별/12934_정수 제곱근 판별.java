11e9e1ea57309418872c93955bede44e1474ddaf
Kate <120708446+kateiscoding@users.noreply.github.com>
2023-03-26 23:52:59 +0900

    Update 12934_정수 제곱근 판별.java

--- Kate/Programmers/Java/12934_정수 제곱근 판별/12934_정수 제곱근 판별.java
+++ Kate/Programmers/Java/12934_정수 제곱근 판별/12934_정수 제곱근 판별.java
@@ -1,6 +1,7 @@
 class Solution {
     public long solution(long n) {
         long x = (long)Math.sqrt(n);
+        // x값에 Math.sqrt를 활용하여 n의 제곱 이전 값을 저장함 
         if (Math.pow(x,2) == n)  {
             return (long)Math.pow(x+1,2);
         }
@@ -9,3 +10,6 @@ class Solution {
             }
     }
 }
+
+
+ 
