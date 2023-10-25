af800bb66702f74fe9d631635e6c15e3bc8117d1
KimEoJin <87289562+greeneryjin@users.noreply.github.com>
2023-10-25 22:53:29 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -5,49 +5,51 @@
  * 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 출력하는 프로그램을 작성하시오.
  * */
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
     static int count = 0;
     static boolean[] visit;
-    static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+    static int[][] arr;
+
     static int n;
-    static int v;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        v = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); //정점
 
-        for (int i = 0; i <= n; i++) {
-            list.add(new ArrayList<>());
+        st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken()); //간선의 개수
+
+        arr = new int[n + 1][n + 1];
+        visit = new boolean[n + 1];
+
+        for (int i = 1; i <= v; i++) { //배열은 1부터 n+1했기 때문에 간선의 개수만큼 넣어야함
+            st = new StringTokenizer(br.readLine());
+            int ln = Integer.parseInt(st.nextToken());
+            int rn = Integer.parseInt(st.nextToken());
+
+            arr[ln][rn] = 1;
+            arr[rn][ln] = 1;
         }
-
-        for (int i = 1; i <= v; i++) {
-            int ln = sc.nextInt();
-            int rn = sc.nextInt();
-
-            list.get(ln).add(rn);
-            list.get(rn).add(ln);
-        }
-
-        visit = new boolean[n+1];
         DFS(1);
-        System.out.println(count);
+        System.out.println(count - 1);
     }
 
     private static void DFS(int index) {
 
+        count++;
         visit[index] = true;
-        for (int i = 0; i < list.get(index).size(); i++) {
-            Integer node = list.get(index).get(i);
-            if(!visit[node]) {
-                count++;
-                DFS(node);
+        for (int i = 1; i <= n; i++) { //행렬이 가진 열만큼 확인
+            if(arr[index][i] == 1 && !visit[i]) { //대칭 행렬이라 좌우, 우좌나 값이 같음
+                DFS(i);
             }
         }
     }
