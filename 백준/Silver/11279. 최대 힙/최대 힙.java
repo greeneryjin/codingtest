04f94f0004f0f49af8561a9765002fbd7f0ce47f
KimEoJin <87289562+greeneryjin@users.noreply.github.com>
2023-11-21 20:08:17 +0900

    [Silver II] Title: 최대 힙, Time: 412 ms, Memory: 31176 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+    public static void main(String[] args) throws IOException {
+        PriorityQueue<Integer> priorityQueueHighest = new PriorityQueue<>(Collections.reverseOrder());
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int data = Integer.parseInt(st.nextToken());
+            if(data == 0) {
+                if(pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.remove()).append("\n");
+                }
+
+            } else {
+                pq.add(data);
+            }
+        }
+
+        System.out.println(sb);
+
+        
+    }
+}
